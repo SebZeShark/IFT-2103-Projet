4c9efdc753e8f6768c2fc59acd8508e7fd150618
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-10-15 09:07:20 -0400

    debut gravité

---
+++ Assets/canon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canon : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newObj = Instantiate(obj, new Vector3(0f, 0f, 0f), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/collision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using myGravity;
+
+public class collision : MonoBehaviour
+{
+    public gravity grav;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(grav.speed.y < -10)
+        {
+            grav.speed.y = -grav.speed.y;
+        }
+        
+    }
+}
