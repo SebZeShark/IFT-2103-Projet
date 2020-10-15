74a86688be72748f67306dfc26daaff46b240854
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-10-15 09:42:49 -0400

    Le cannon peut tirer un cube

--- Assets/canon.cs
+++ Assets/canon.cs
@@ -1,20 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using myGravity;
 
 public class canon : MonoBehaviour
 {
+    gravity grav;
     public GameObject obj;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject newObj = Instantiate(obj, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        Instantiate(obj, new Vector3(0f, 0f, 0f), Quaternion.identity);
 
+        grav = obj.GetComponent<gravity>();
+
+        grav.speed = new Vector3(0, 15, 15);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (collision.num < 1)
+            {
+                Instantiate(obj, new Vector3(0f, 0f, 0f), Quaternion.identity);
+
+                obj.GetComponent<gravity>().speed = new Vector3(0, 15, 15);
+            }
+        }
         
     }
 }
--- Assets/collision.cs
+++ Assets/collision.cs
@@ -6,19 +6,29 @@ using myGravity;
 public class collision : MonoBehaviour
 {
     public gravity grav;
+    static public int num;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (num >= 0)
+            num++;
+        else
+            num = 0;
+
+        Debug.Log(num);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(grav.speed.y < -10)
+        if(transform.position.y <= 0)
         {
             grav.speed.y = -grav.speed.y;
         }
-        
+        if(transform.position.z > 100)
+        {
+            num--;
+            Destroy(gameObject);
+        }
     }
 }
