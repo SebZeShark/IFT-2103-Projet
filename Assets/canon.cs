4e55c391959cc02c0e9c28e4461772bec0386be0
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-10-16 20:05:54 -0400

    modif de cannon: ajout d'un slider de force et du choix de forme

--- Assets/canon.cs
+++ Assets/canon.cs
@@ -2,74 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using myGravity;
+using UnityEngine.UI;
 
 public class canon : MonoBehaviour
 {
-    gravity grav;
-    public GameObject obj;
-    int idJoueur;
-    int idForme;
-    int angle;
-    int force;
-    bool canThrow;
-    int winner;
+    public GameObject cube;
+    public GameObject sphere;
+    public Slider force;
+    public Dropdown dd;
+    GameObject obj;
     
     // Start is called before the first frame update
     void Start()
     {
-        grav = obj.GetComponent<gravity>();
-        idJoueur = 1;
-        idForme = 0;
-        angle = 0;
-        force = 0;
-        canThrow = false;
-
-        winner = boucleDeJeu();
+        obj = cube;
+        obj.GetComponent<gravity>().speed = new Vector3(0, 25 * 0, 25 * 0);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (idForme == 0)
-            idForme = selectForm();
-        else
+    { 
+        if (Input.GetButtonDown("Jump"))
         {
-            if (Input.GetButtonDown("Jump") && canThrow)
+            if (collision.num < 10)
             {
-                if (collision.num < 1)
-                {
-                    Instantiate(obj, new Vector3(0f, 0f, 0f), Quaternion.identity);
+                Instantiate(obj, transform.position, Quaternion.identity);
 
-                    obj.GetComponent<gravity>().speed = new Vector3(0, 10, 10);
-                }
             }
-
         }
     }
 
-    int boucleDeJeu()
+    public void onChange(System.Single newValue)
     {
-        idForme = selectForm();
-        angle = choisirAngle();
-        force = choisirForce();
-        canThrow = true;
 
-        return idJoueur;
+        obj.GetComponent<gravity>().speed = new Vector3(0, 25 * newValue, 25 * newValue);
     }
 
-    int selectForm()
+    public void formOnChange(int val)
     {
-        return 1;
-    }
-
-    int choisirAngle()
-    {
-        return 45;
-    }
-
-    int choisirForce()
-    {
-        return 50;
+        switch (val)
+        {
+            case 0:
+                obj = cube;
+                break;
+            case 1:
+                obj = sphere;
+                break;
+            default:
+                obj = cube;
+                break;
+        }
     }
 
 }
