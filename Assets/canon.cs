1fb548bd9a9ddec5b476183a672991789fddaafc
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-10-16 21:29:55 -0400

    angles qui marchent pas :(

--- Assets/canon.cs
+++ Assets/canon.cs
@@ -9,33 +9,43 @@ public class canon : MonoBehaviour
     public GameObject cube;
     public GameObject sphere;
     public Slider force;
+    public Slider angle;
     public Dropdown dd;
+    float forceV;
+    int angleV;
     GameObject obj;
     
     // Start is called before the first frame update
     void Start()
     {
         obj = cube;
-        obj.GetComponent<gravity>().speed = new Vector3(0, 25 * 0, 25 * 0);
+        forceV = 0;
+        angleV = 0;
+        obj.GetComponent<gravity>().speed = new Vector3(0, 25 * forceV * Mathf.Cos((angleV * Mathf.PI) / 180), 25 * forceV * Mathf.Sin((angleV * Mathf.PI) / 180));
     }
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+        obj.GetComponent<gravity>().speed = new Vector3(0, 25 * forceV * Mathf.Sin((angleV * Mathf.PI) /180), 25 * forceV * Mathf.Cos((angleV * Mathf.PI) / 180));
+
         if (Input.GetButtonDown("Jump"))
         {
             if (collision.num < 10)
             {
                 Instantiate(obj, transform.position, Quaternion.identity);
-
             }
         }
     }
 
     public void onChange(System.Single newValue)
     {
+        forceV = newValue;
+    }
 
-        obj.GetComponent<gravity>().speed = new Vector3(0, 25 * newValue, 25 * newValue);
+    public void angleOnChange(int newValue)
+    {
+        angleV = newValue;
     }
 
     public void formOnChange(int val)
