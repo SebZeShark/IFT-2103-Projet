4fa95c718e1e4e6eba1a846dc8dfc914186c5d10
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-10-17 12:13:14 -0400

    UI fini

--- Assets/canon.cs
+++ Assets/canon.cs
@@ -11,6 +11,10 @@ public class canon : MonoBehaviour
     public Slider force;
     public Slider angle;
     public Dropdown dd;
+    public Button tirer;
+    public Text forceText;
+    public Text angleText;
+    public Text formeText;
     float forceV;
     int angleV;
     GameObject obj;
@@ -20,32 +24,31 @@ public class canon : MonoBehaviour
     void Start()
     {
         obj = cube;
-        forceV = 0;
-        angleV = 180;
+        forceV = 0.05f;
+        angleV = 0;
+        forceText.text = (Mathf.FloorToInt(forceV * 100)).ToString();
+        angleText.text = angleV.ToString();
+        formeText.text = "Cube";
     }
 
     // Update is called once per frame
     void Update()
     {
-        obj.GetComponent<gravity>().speed = new Vector3(0, 25 * forceV * Mathf.Sin((angleV * Mathf.PI) /180), 25 * forceV * Mathf.Cos((angleV * Mathf.PI) / 180));
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (collision.num < 10)
-            {
-                Instantiate(obj, transform.position, Quaternion.identity);
-            }
-        }
+        obj.GetComponent<gravity>().speed = new Vector3(0, 
+            25 * forceV * Mathf.Sin((angleV * Mathf.PI) /180),
+            25 * forceV * Mathf.Cos((angleV * Mathf.PI) / 180));
     }
 
     public void onChange(System.Single newValue)
     {
         forceV = newValue;
+        forceText.text =  (Mathf.FloorToInt(forceV*100)).ToString();
     }
 
     public void angleOnChange(System.Single newValue)
     {
         angleV = (int) newValue;
+        angleText.text = angleV.ToString();
     }
 
     public void formOnChange(int val)
@@ -54,14 +57,25 @@ public class canon : MonoBehaviour
         {
             case 0:
                 obj = cube;
+                formeText.text = "Cube";
                 break;
             case 1:
                 obj = sphere;
+                formeText.text = "Sphère";
                 break;
             default:
                 obj = cube;
+                formeText.text = "Cube";
                 break;
         }
+
     }
 
+    public void tirerOnClick()
+    {
+        if (collision.num < 10)
+        {
+            Instantiate(obj, transform.position + new Vector3(0, 0.5f, 0.5f), Quaternion.identity);
+        }
+    }
 }
