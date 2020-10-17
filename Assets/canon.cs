a6471405a36a13a9c47875e80ec051bb6a5c13ed
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-10-16 21:44:10 -0400

    new canon

--- Assets/canon.cs
+++ Assets/canon.cs
@@ -20,14 +20,10 @@ public class canon : MonoBehaviour
     void Start()
     {
         obj = cube;
-<<<<<<< HEAD
         forceV = 0;
         angleV = 0;
         obj.GetComponent<gravity>().speed = new Vector3(0, 25 * forceV * Mathf.Cos((angleV * Mathf.PI) / 180), 25 * forceV * Mathf.Sin((angleV * Mathf.PI) / 180));
-=======
-        obj.GetComponent<gravity>().speed = new Vector3(0, 25 * 0, 25 * 0);
         target = new Vector3(0, 15, 15);
->>>>>>> 825926fa6b96475d16f0068d6301894e46521afb
     }
 
     // Update is called once per frame
@@ -40,26 +36,18 @@ public class canon : MonoBehaviour
             if (collision.num < 10)
             {
                 Instantiate(obj, transform.position, Quaternion.identity);
-<<<<<<< HEAD
-=======
-                obj.GetComponent<gravity>().speed = target;
->>>>>>> 825926fa6b96475d16f0068d6301894e46521afb
             }
         }
     }
 
     public void onChange(System.Single newValue)
     {
-<<<<<<< HEAD
         forceV = newValue;
     }
 
     public void angleOnChange(int newValue)
     {
         angleV = newValue;
-=======
-        target = new Vector3(0, 25 * newValue, 25 * newValue);
->>>>>>> 825926fa6b96475d16f0068d6301894e46521afb
     }
 
     public void formOnChange(int val)
