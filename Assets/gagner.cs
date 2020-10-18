40c3a040c72e290adfefb35b2f56cb6ad53e4f5b
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-10-18 11:51:04 -0400

    test, j'ai tout ajouté

---
+++ Assets/gagner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using myGravity;
+
+public class gagner : MonoBehaviour
+{
+    public gravity grav;
+    Transform dataShape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
